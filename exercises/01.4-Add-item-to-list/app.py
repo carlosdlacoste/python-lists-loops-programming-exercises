06fa2c14a9578ce92079ef97b6b4b6faa26e17eb
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-23 18:15:26 +0000

    Resueltos los ejercicios hasta el numero 4

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,11 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(10):
+    random_numbers = random.randint(1, 10)
+    my_list.append(random_numbers)
+
+print(my_list)
--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,7 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
-
+new_list = []
 #your code below:
+for i in reversed(arr):
+    new_list.append(i)
+
+print(new_list)
